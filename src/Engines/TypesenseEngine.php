ef76d2b75faa436e43a16bdf7f7a97784559aebe
Samuel Hautcoeur <samuel.hautcoeur@ep4.com>
2022-11-08 16:39:59 +0000

    fix: filters function return ' && ' when wheres and whereIns is empty

--- src/Engines/TypesenseEngine.php
+++ src/Engines/TypesenseEngine.php
@@ -395,7 +395,9 @@ class TypesenseEngine extends Engine
             ->values()
             ->implode(' && ');
 
-		return $whereFilter . ' && ' . $whereInFilter;
+        return $whereFilter . (
+            (strlen($whereFilter) > 0 && strlen($whereInFilter) > 0) ? ' && ' : ''
+        ) . $whereInFilter;
     }
 
     /**
